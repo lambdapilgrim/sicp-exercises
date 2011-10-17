07bd51c29832f505294941f45c1fa7a5264b2a2f
Animesh Kumar <imagine.animesh@gmail.com>
2011-10-17 16:51:52 -0500

    readjusted for more readability

--- chapter1/1_4.rkt
+++ chapter1/1_4.rkt
@@ -4,7 +4,9 @@
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
   
-;Based on whether b is negative or not, the right operator is used to oeprate on a and b. An addition on a and b will be performed if b is a positive number, else subtraction of a and b will be performed.
+;Based on whether b is negative or not, the right operator is used to oeprate on
+;a and b. An addition on a and b will be performed if b is a positive number,
+;else subtraction of a and b will be performed.
 
 (a-plus-abs-b 5 6)
 
