d0b290c103cdac1de4e61aea25fce5f855720b46
Animesh Kumar <imagine.animesh@gmail.com>
2011-10-17 16:49:48 -0500

    readjusted for more readability

--- chapter1/1_5.rkt
+++ chapter1/1_5.rkt
@@ -9,6 +9,15 @@
       y))
 
 (test 0 (p))
-;If the interpreter uses normal-order evaluation, then the execution will complete with the answer 0. If it uses applicative-order evaluation, then the process will not terminate.
+;If the interpreter uses normal-order evaluation, then the execution will
+;complete with answer 0. If it uses applicative-order evaluation, then the
+;process will not terminate.
 
-;This is because in applicative order evaluation, the operands are first evaluated before the operator is applied on them. Since evaluation of (p) will be performed in applicative-order evaluation, the process will never terminate. On the other hand, in normal-order evaluation, "fully expand and then reduce" evaluation method will be applied. This will yield to the if expression, which readily reduces to 0. As per the assumption in the question, evaluation rule for if is that the predicate expression is evaluated first, and if it evaluates to true, consequence is returned and alternative is not evaluated.
+;This is because in applicative order evaluation, the operands are first
+;evaluated before the operator is applied on them. Since evaluation of (p)
+;will be performed in this case, the process will never terminate. On the
+;other hand, normal-order evaluation("fully expand and then reduce") will
+;yield to the if expression, which readily reduces to 0. As per the assumption
+;in the question, evaluation rule for if is that the predicate is evaluated
+;first, if it evaluates to true, consequent is returned and alternative is not
+;evaluated.
